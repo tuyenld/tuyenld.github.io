707f1c19f8fc982753e265ead3ef5866383bbf2b
tuyenld <ledinhtuyen.hust@gmail.com>
2019-07-06 21:29:18 +0700

    Move to TeXt Theme

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,58 +1,29 @@
----
-layout: null
----
-
 var store = [
-  {%- for c in site.collections -%}
-    {%- if forloop.last -%}
-      {%- assign l = true -%}
-    {%- endif -%}
-    {%- assign docs = c.docs | where_exp:'doc','doc.search != false' -%}
-    {%- for doc in docs -%}
-      {%- if doc.header.teaser -%}
-        {%- capture teaser -%}{{ doc.header.teaser }}{%- endcapture -%}
-      {%- else -%}
-        {%- assign teaser = site.teaser -%}
-      {%- endif -%}
-      {% comment %} ldtuyen | tregiengchan {% endcomment %}
-      {% if doc.exclude_from_search %}
-        {% continue %}
-      {% endif %}
+      
       {
-        "title": {{ doc.title | jsonify }},
-        "excerpt":
-          {%- if site.search_full_content == true -%}
-            {{ doc.content | newline_to_br |
-              replace:"<br />", " " |
-              replace:"</p>", " " |
-              replace:"</h1>", " " |
-              replace:"</h2>", " " |
-              replace:"</h3>", " " |
-              replace:"</h4>", " " |
-              replace:"</h5>", " " |
-              replace:"</h6>", " "|
-            strip_html | strip_newlines | jsonify }},
-          {%- else -%}
-            {{ doc.content | newline_to_br |
-              replace:"<br />", " " |
-              replace:"</p>", " " |
-              replace:"</h1>", " " |
-              replace:"</h2>", " " |
-              replace:"</h3>", " " |
-              replace:"</h4>", " " |
-              replace:"</h5>", " " |
-              replace:"</h6>", " "|
-            strip_html | strip_newlines | truncatewords: 50 | jsonify }},
-          {%- endif -%}
-        "categories": {{ doc.categories | jsonify }},
-        "tags": {{ doc.tags | jsonify }},
-        "url": {{ doc.url | absolute_url | jsonify }},
-        "teaser":
-          {%- if teaser contains "://" -%}
-            {{ teaser | jsonify }}
-          {%- else -%}
-            {{ teaser | absolute_url | jsonify }}
-          {%- endif -%}
-      }{%- unless forloop.last and l -%},{%- endunless -%}
-    {%- endfor -%}
-  {%- endfor -%}]
+        "title": "Gửi thanh niên An Nam",
+        "excerpt":"Ông Đu-Me, nguyên toàn quyền Đông Dương đã viết: “Khi nước Pháp đến Đông Dương, thì dân tộc Việt Nam đã chín muồi để làm nô lệ”. Từ đó đến nay hơn một nửa thế kỷ đã trôi qua. Nhiều biến cố phi thường đã làm đảo lộn thế giới....","categories": ["Life"],
+        "tags": ["Văn học"],
+        "url": "https://crushcoding.com/life/gui-thanh-nien-an-nam",
+        "teaser":null},
+      
+      {
+        "title": "Upgrade flash chip on TP-Link from 4mb to 8mb",
+        "excerpt":"1. Pre-requirement New flash chip: I used Winbond 8MiB GD25Q64CSIG Soldering iron: desolder old flash chip and re-solder new flash chip ROM programer and IC socket: I am using CH-341A (~ 5$) One PC/Laptop 2. Let’s do it 2.1 Desolder old flash chip Hot air soldering station is ideally suited...","categories": ["OpenWRT"],
+        "tags": ["TL-WR840N"],
+        "url": "https://crushcoding.com/openwrt/upgrade-flash-chip-tplink-tl-wr840n-4mb-to-8mb",
+        "teaser":null},
+      
+      {
+        "title": "Người lạ - Mai Thảo Yên",
+        "excerpt":"Tôi bắt gặp Người lạ trong bài giới thiệu Văn học tuổi 20 của báo Tuổi trẻ. Ở cái tuổi của tác giả và của tôi thì những suy nghĩ về cuộc đời, những câu hỏi không tìm được lời giải khá là rõ nét. Câu chuyện bắt đầu với...","categories": ["Life"],
+        "tags": ["Văn học"],
+        "url": "https://crushcoding.com/life/nguoi-la-mai-thao-yen",
+        "teaser":null},
+      
+      {
+        "title": "How to use Google Analytics API",
+        "excerpt":"I want to show pageview on my blog. One problem is this blog is static blog (powered by Jekyll), so I think about use Google Analytics to do that. I found a ton of document, there are many keyword on Google Cloud Product. Services Account Access Token Fresh Token OAuth...","categories": ["blog"],
+        "tags": ["blog","jekyll"],
+        "url": "https://crushcoding.com/blog/how-to-use-google-analytics-api",
+        "teaser":null}]

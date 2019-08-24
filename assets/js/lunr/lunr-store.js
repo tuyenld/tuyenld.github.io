a3dff214773655128cbdc3a24eda8bc77fc73f79
tuyenld <ledinhtuyen.hust@gmail.com>
2019-08-24 20:26:11 +0700

    Mot nghe cho chin con hon chin nghe

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -17,13 +17,13 @@ var store = [
       {
         "title": "Người lạ - Mai Thảo Yên",
         "excerpt":"Tôi bắt gặp Người lạ trong bài giới thiệu Văn học tuổi 20 của báo Tuổi trẻ. Ở cái tuổi của tác giả và của tôi thì những suy nghĩ về cuộc đời, những câu hỏi không tìm được lời giải khá là rõ nét. Câu chuyện bắt đầu với...","categories": ["Life"],
-        "tags": ["Văn học","Schoolarship"],
+        "tags": ["Văn học","Schoolarship","Book"],
         "url": "https://crushcoding.com/life/nguoi-la-mai-thao-yen",
         "teaser":null},
       
       {
-        "title": "Kinh nghiệm du lịch Brazil",
-        "excerpt":"Đặt tiêu đề cho hay vậy thôi, chứ mình có đi du lịch đâu, business đàng hoàng đấy chứ. Anh đã làm mọi thứ để không phải đi, vì anh biết rằng chuyến đó sẽ chẳng dễ dàng gì. Support xuyên đêm anh cũng đã thử, anh đã cố nghĩ...","categories": ["life"],
+        "title": "Brazil bâng quơ",
+        "excerpt":"Anh đã làm mọi thứ để không phải đi, vì anh biết rằng chuyến đó sẽ chẳng dễ dàng gì. Support xuyên đêm anh cũng đã thử, anh đã cố nghĩ hết các trường hợp có thể xảy ra, nhưng không có kết quả. Và rồi cái gì đến cũng...","categories": ["life"],
         "tags": ["trip"],
         "url": "https://crushcoding.com/life/brazil-co-don-truong-thanh",
         "teaser":null},
@@ -33,4 +33,25 @@ var store = [
         "excerpt":"I want to show pageview on my blog. One problem is this blog is static blog (powered by Jekyll), so I think about use Google Analytics to do that. I found a ton of document, there are many keyword on Google Cloud Product. Services Account Access Token Fresh Token OAuth...","categories": ["blog"],
         "tags": ["blog","jekyll"],
         "url": "https://crushcoding.com/blog/how-to-use-google-analytics-api",
+        "teaser":null},
+      
+      {
+        "title": "Học Khoa Học Máy Tính nên đọc sách gì",
+        "excerpt":"Nguồn - Blog Khoa Học Máy Tính Học Khoa Học Máy Tính nên đọc sách gì? By NQH Published: 01/10/2007 Bạn trantempo hỏi một danh sách các sách nên đọc trong KHMT. Bạn Nguyên đã đưa một danh sách sơ bộ. Tôi sẽ cập nhật từ từ vào danh sách...","categories": ["Life"],
+        "tags": ["Book"],
+        "url": "https://crushcoding.com/life/hoc-khmt-nen-doc-sach-gi",
+        "teaser":null},
+      
+      {
+        "title": "Làm an toàn thông tin thì học gì",
+        "excerpt":"Nguồn - thái Làm an toàn thông tin thì học gì? May 02, 2012 1 Giới thiệu Tôi nhận được thư từ của nhiều bạn hỏi về việc nên học gì và như thế nào để có thể tìm được việc làm và làm được việc trong ngành an toàn...","categories": ["Life"],
+        "tags": ["Book"],
+        "url": "https://crushcoding.com/life/lam-attt-thi-hoc-gi",
+        "teaser":null},
+      
+      {
+        "title": "Một nghề cho chín còn hơn chín nghề",
+        "excerpt":"Nguồn - thái người ta nói, cách tốt nhất để ăn một con voi, là ăn một miếng thịt của nó mỗi ngày. ngẫm lại chuyện học, chuyện làm, mình thấy đây là một lời khuyên có lý. tri thức thì bao la vô tận, không thể nào ngày một...","categories": ["Life"],
+        "tags": ["No tags"],
+        "url": "https://crushcoding.com/life/mot-nghe-cho-chin-con-hon-chin-nghe",
         "teaser":null}]

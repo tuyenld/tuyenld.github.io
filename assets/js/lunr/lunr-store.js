a8ca40b616d5ee249eb3e1c1dafbdaacbfef552e
tuyenld <tuyenld.work@gmail.com>
2020-08-22 23:23:04 +0700

    Git revert Centered post - jekyll-TeXt-theme

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,64 @@
+var store = [
+      
+      {
+        "title": "Gửi thanh niên An Nam",
+        "excerpt":"Ông Đu-Me, nguyên toàn quyền Đông Dương đã viết: “Khi nước Pháp đến Đông Dương, thì dân tộc Việt Nam đã chín muồi để làm nô lệ”. Từ đó đến nay hơn một nửa thế kỷ đã trôi qua. Nhiều biến cố phi thường đã làm đảo lộn thế giới....","categories": ["Life"],
+        "tags": ["Văn học"],
+        "url": "https://crushcoding.com/life/gui-thanh-nien-an-nam",
+        "teaser":null},
+      
+      {
+        "title": "Upgrade flash chip on TP-Link from 4mb to 8mb",
+        "excerpt":"1. Pre-requirement New flash chip: I used Winbond 8MiB GD25Q64CSIG Soldering iron: desolder old flash chip and re-solder new flash chip ROM programer and IC socket: I am using CH-341A (~ 5$) One PC/Laptop 2. Let’s do it 2.1 Desolder old flash chip Hot air soldering station is ideally suited...","categories": ["OpenWRT"],
+        "tags": ["TL-WR840N"],
+        "url": "https://crushcoding.com/openwrt/upgrade-flash-chip-tplink-tl-wr840n-4mb-to-8mb",
+        "teaser":null},
+      
+      {
+        "title": "Người lạ - Mai Thảo Yên",
+        "excerpt":"Tôi bắt gặp Người lạ trong bài giới thiệu Văn học tuổi 20 của báo Tuổi trẻ. Ở cái tuổi của tác giả và của tôi thì những suy nghĩ về cuộc đời, những câu hỏi không tìm được lời giải khá là rõ nét. Câu chuyện bắt đầu với...","categories": ["Life"],
+        "tags": ["Văn học","Schoolarship","Book"],
+        "url": "https://crushcoding.com/life/nguoi-la-mai-thao-yen",
+        "teaser":null},
+      
+      {
+        "title": "Brazil bâng quơ",
+        "excerpt":"Anh đã làm mọi thứ để không phải đi, vì anh biết rằng chuyến đó sẽ chẳng dễ dàng gì. Support xuyên đêm anh cũng đã thử, anh đã cố nghĩ hết các trường hợp có thể xảy ra, nhưng không có kết quả. Và rồi cái gì đến cũng...","categories": ["life"],
+        "tags": ["trip"],
+        "url": "https://crushcoding.com/life/brazil-co-don-truong-thanh",
+        "teaser":null},
+      
+      {
+        "title": "How to use Google Analytics API",
+        "excerpt":"I want to display pageview on my blog (you can see the “eye” icon near the date of the post). One problem is this blog is static blog (powered by Jekyll), so I think about use Google Analytics to do that. I found a ton of document, there are many...","categories": ["blog"],
+        "tags": ["blog","jekyll"],
+        "url": "https://crushcoding.com/blog/how-to-use-google-analytics-api",
+        "teaser":null},
+      
+      {
+        "title": "Học Khoa Học Máy Tính nên đọc sách gì",
+        "excerpt":"Bài viết gốc - Blog Khoa Học Máy Tính Bạn trantempo hỏi một danh sách các sách nên đọc trong KHMT. Bạn Nguyên đã đưa một danh sách sơ bộ. Tôi sẽ cập nhật từ từ vào danh sách dưới đây trong một vài tuần tới để có một danh...","categories": ["Life"],
+        "tags": ["Book"],
+        "url": "https://crushcoding.com/life/hoc-khmt-nen-doc-sach-gi",
+        "teaser":null},
+      
+      {
+        "title": "Làm an toàn thông tin thì học gì",
+        "excerpt":"Bài viết gốc - thái, May 02, 2012 1. Giới thiệu Tôi nhận được thư từ của nhiều bạn hỏi về việc nên học gì và như thế nào để có thể tìm được việc làm và làm được việc trong ngành an toàn thông tin (information security). An toàn...","categories": ["Life"],
+        "tags": ["Book"],
+        "url": "https://crushcoding.com/life/lam-attt-thi-hoc-gi",
+        "teaser":null},
+      
+      {
+        "title": "Một nghề cho chín còn hơn chín nghề",
+        "excerpt":"Nguồn - thái người ta nói, cách tốt nhất để ăn một con voi, là ăn một miếng thịt của nó mỗi ngày. ngẫm lại chuyện học, chuyện làm, mình thấy đây là một lời khuyên có lý. tri thức thì bao la vô tận, không thể nào ngày một...","categories": ["Life"],
+        "tags": ["No tags"],
+        "url": "https://crushcoding.com/life/mot-nghe-cho-chin-con-hon-chin-nghe",
+        "teaser":null},
+      
+      {
+        "title": "Cứ đi rồi sẽ đến... [Chuyện đi học]",
+        "excerpt":"Source: Notes by Vy Vy Le Tháng 7… Tâm điểm của những ngày hè oi bức ở Hà Nội Tháng 7 của tôi hôm nay Nó không còn là tháng 7 của 7 năm về trước khi tất cả những đứa 18 tuổi phải vật lộn bước vào kỳ thi...","categories": ["Schoolarship"],
+        "tags": ["Schoolarship"],
+        "url": "https://crushcoding.com/schoolarship/cu-di-roi-se-den-chuyen-di-hoc",
+        "teaser":null}]
